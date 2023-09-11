b9b0df2d1ea9a614e1bd56a3975883276a8bea9e
Tiago Moreira <tiagomoreira317@gmail.com>
2023-09-11 18:15:28 +0100

    Fixed taskTimeAgo func to include months and days

--- utils/funcs.py
+++ utils/funcs.py
@@ -5,22 +5,27 @@ def taskTimeAgo(tdd: dict) -> str:
             cardTimeLabel = "Created "
             dateCreated = dt.strptime(tdd[col][task][1], '%Y-%m-%d %H:%M:%S.%f')
             timeAgo = str(dt.now() - dateCreated).split(":")
-            if int(timeAgo[0]) > 0:
-                if int(timeAgo[0]) == 1:
-                    cardTimeLabel += "1 hour ago"
-                else:
-                    cardTimeLabel += f"{int(timeAgo[0])} hours ago"
+            if "day" in timeAgo:
+                cardTimeLabel += f"{timeAgo.split(',')[0]} ago"
+            elif "month" in timeAgo:
+                cardTimeLabel += f"{timeAgo.split(',')[0]} and {timeAgo.split(',')[1].strip()} ago"
             else:
-                if int(timeAgo[1]) > 0:
-                    if int(timeAgo[1]) == 1:
-                        cardTimeLabel += "1 minute ago"
+                if int(timeAgo[0]) > 0:
+                    if int(timeAgo[0]) == 1:
+                        cardTimeLabel += "1 hour ago"
                     else:
-                        cardTimeLabel += f"{int(timeAgo[1])} minutes ago"
+                        cardTimeLabel += f"{int(timeAgo[0])} hours ago"
                 else:
-                    if int(timeAgo[2].split(".")[0]) == 1:
-                        cardTimeLabel += "1 second ago"
+                    if int(timeAgo[1]) > 0:
+                        if int(timeAgo[1]) == 1:
+                            cardTimeLabel += "1 minute ago"
+                        else:
+                            cardTimeLabel += f"{int(timeAgo[1])} minutes ago"
                     else:
-                        cardTimeLabel += f"{int(timeAgo[2].split('.')[0])} seconds ago" 
+                        if int(timeAgo[2].split(".")[0]) == 1:
+                            cardTimeLabel += "1 second ago"
+                        else:
+                            cardTimeLabel += f"{int(timeAgo[2].split('.')[0])} seconds ago" 
             tdd[col][task][1] = cardTimeLabel             
     return tdd
     
