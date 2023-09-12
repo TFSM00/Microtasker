fdca3b4fb3580123d94140f8ed0226c818e2d4c5
Tiago Moreira <tiagomoreira317@gmail.com>
2023-09-12 19:51:58 +0100

    Fixed register form theme input

--- utils/forms.py
+++ utils/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField, SelectField, TextAreaField
+from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField, RadioField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
-from flask_ckeditor import CKEditorField
 
 class RegisterForm(FlaskForm):
     username = StringField(label='Username', validators=[DataRequired()])
     email = EmailField(label='Email', validators=[Email()])
-    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
-    theme = SelectField(label="Select preferred theme (can be changed later)", choices=[(True, "Light"), (False, "Dark")], validate_choice=True)
+    password = PasswordField(label='Password', validators=[Length(min=5)])
+    theme = RadioField(label="Select preferred theme", choices=[("Light"), ("Dark")], coerce=bool)
     submit = SubmitField(label="Register")
 
 class LoginForm(FlaskForm):
