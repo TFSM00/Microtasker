524eeaa5633a42e85a3f64d1cbb7f9e46222d702
Tiago Moreira <tiagomoreira317@gmail.com>
2023-09-18 20:00:30 +0100

    Created function to show time ago in cards

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ import datetime as dt
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-
 from flask_session import Session
+
 from models import db
+from utils.funcs import time_ago
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///micro.db'
-    app.config['SECRET_KEY'] = 'key'    
+    app.config['SECRET_KEY'] = 'key'
     app.config['SESSION_PERMANENT'] = False
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['PERMANENT_SESSION_LIFETIME'] = dt.timedelta(hours=5)
+
+    app.jinja_env.globals['timeago'] = time_ago
+
     Bootstrap(app)
     app.app_context().push()
     Session(app)
--- utils/funcs.py
+++ utils/funcs.py
@@ -1,31 +1,36 @@
-from datetime import datetime as dt
-def taskTimeAgo(tdd: dict) -> str:
-    for col in tdd:
-        for task in tdd[col]:
-            cardTimeLabel = "Created "
-            dateCreated = dt.strptime(tdd[col][task][1], '%Y-%m-%d %H:%M:%S.%f')
-            timeAgo = str(dt.now() - dateCreated).split(":")
-            if "day" in timeAgo:
-                cardTimeLabel += f"{timeAgo.split(',')[0]} ago"
-            elif "month" in timeAgo:
-                cardTimeLabel += f"{timeAgo.split(',')[0]} and {timeAgo.split(',')[1].strip()} ago"
-            else:
-                if int(timeAgo[0]) > 0:
-                    if int(timeAgo[0]) == 1:
-                        cardTimeLabel += "1 hour ago"
-                    else:
-                        cardTimeLabel += f"{int(timeAgo[0])} hours ago"
-                else:
-                    if int(timeAgo[1]) > 0:
-                        if int(timeAgo[1]) == 1:
-                            cardTimeLabel += "1 minute ago"
-                        else:
-                            cardTimeLabel += f"{int(timeAgo[1])} minutes ago"
-                    else:
-                        if int(timeAgo[2].split(".")[0]) == 1:
-                            cardTimeLabel += "1 second ago"
-                        else:
-                            cardTimeLabel += f"{int(timeAgo[2].split('.')[0])} seconds ago" 
-            tdd[col][task][1] = cardTimeLabel             
-    return tdd
-    
+"""
+Module containing useful functions for the website
+"""
+import datetime as dt
+import re
+
+
+def time_ago(time: str) -> str:
+    """
+    Returns a string showing how long ago the inputted time string was.
+    """
+    formatted_dt = dt.datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
+    time_ago = re.split(r"[:\.\,]+", str(dt.datetime.utcnow() - formatted_dt))
+
+    if len(time_ago) > 4:
+        return f"{time_ago[0]} ago"
+
+    if int(time_ago[-4]) > 0:
+        if int(time_ago[-4]) == 1:
+            return f"{int(time_ago[-4])} hour ago"
+        return f"{int(time_ago[-4])} hours ago"
+
+    if int(time_ago[-3]) > 0:
+        if int(time_ago[-3]) == 1:
+            return f"{int(time_ago[-3])} minute ago"
+        return f"{int(time_ago[-3])} minutes ago"
+
+    return "Just now"
+
+
+if __name__ == '__main__':
+    class Obj:
+        def __init__(self) -> None:
+            self.date_created = "2023-09-18 19:00:20.448339"
+    obj = Obj()
+    print(time_ago(object.date_created))
