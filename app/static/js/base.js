323ecf2e815338c46fecc5ef8fda061c5cafbcf2
Tiago Moreira <tiagomoreira317@gmail.com>
2023-09-27 16:34:11 +0100

    Removed logic present in python

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -3,17 +3,17 @@ if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
   } else {
     document.getElementById('color-theme').setAttribute('class', "bi bi-brightness-high-fill")
   }
-  document.getElementById('btnSwitch').addEventListener('click',()=>{
-  if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-      document.documentElement.setAttribute('data-bs-theme','light')
-      document.getElementById('color-theme').setAttribute('class', "bi bi-brightness-high-fill")
+//   document.getElementById('btnSwitch').addEventListener('click',()=>{
+//   if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+//       document.documentElement.setAttribute('data-bs-theme','light')
+//       document.getElementById('color-theme').setAttribute('class', "bi bi-brightness-high-fill")
 
-  }
-  else {
-      document.documentElement.setAttribute('data-bs-theme','dark')
-      document.getElementById('color-theme').setAttribute('class', "bi bi-moon-stars-fill")
-  }
-})
+//   }
+//   else {
+//       document.documentElement.setAttribute('data-bs-theme','dark')
+//       document.getElementById('color-theme').setAttribute('class', "bi bi-moon-stars-fill")
+//   }
+// })
 
 // Change dropdown button color
 // const dropdown = document.getElementById('dropdown-1');
