e4476f80a88c7314a2e72b27d3a67f3220ddf65d
Tiago Moreira <tiagomoreira317@gmail.com>
2023-09-29 21:20:24 +0100

    Attempt to remove db lag

--- app/run.py
+++ app/run.py
@@ -2,4 +2,7 @@ from main import app
 from os import environ
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=environ.get('DEBUG') == '1')
+    app.run(host='0.0.0.0',
+            port=5000,
+            debug=environ.get('DEBUG') == '1',
+            threaded=True)
